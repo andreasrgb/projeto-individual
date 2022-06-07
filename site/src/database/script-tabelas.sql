b6f5ab906511ccf0df606eb4d1a6c4a818305b5a
Andreas Rodrigues <andreas.bittencourt@sptech.school>
2022-06-07 14:05:42 -0300

    estilizacao

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -25,17 +25,6 @@ foreign key (fk_clube)
 references clubes (id_clube)
 );
 
-insert into usuarios values
-(null, 'Carlos', 'carlos@gmail.com', '123456', 1, '2'),
-(null, 'Mauricio', 'mauricio@gmail.com', '123456', 2, '3'),
-(null, 'Jefferson', 'jefferson@gmail.com', '123456', 3, '4'),
-(null, 'Paulo', 'paulo@gmail.com', '123456', 4, '5'),
-(null, 'Matheus', 'matheus@gmail.com', '123456', 6, '6'),
-(null, 'Juninho', 'Junior@gmail.com', '123456', 7, '7'),
-(null, 'Pedro', 'pedro@gmail.com', '123456', 8, '8'),
-(null, 'Lucas', 'lucas@gmail.com', '123456', 9, '9'),
-(null, 'João', 'joao@gmail.com', '123456', 10, '10');
-
 insert into jogadores values 
 (null, 'Neymar JR'),
 (null, 'Kylian Mbappe'),
@@ -59,6 +48,19 @@ insert into clubes values
 (null, 'Paris Saint-Germain'),
 (null, 'Atlético de Madrid'),
 (null, 'Juventus');
+
+insert into usuarios values
+(null, 'Carlos', 'carlos@gmail.com', '123456', 1, '2'),
+(null, 'Mauricio', 'mauricio@gmail.com', '123456', 2, '3'),
+(null, 'Jefferson', 'jefferson@gmail.com', '123456', 3, '4'),
+(null, 'Paulo', 'paulo@gmail.com', '123456', 4, '5'),
+(null, 'Yucatan', 'yucatan@gmail.com', '123456', 5, '1'),
+(null, 'Matheus', 'matheus@gmail.com', '123456', 6, '6'),
+(null, 'Juninho', 'Junior@gmail.com', '123456', 7, '7'),
+(null, 'Pedro', 'pedro@gmail.com', '123456', 8, '8'),
+(null, 'Lucas', 'lucas@gmail.com', '123456', 9, '9'),
+(null, 'João', 'joao@gmail.com', '123456', 10, '10');
+
 
 select * from usuarios;
 select * from jogadores;
